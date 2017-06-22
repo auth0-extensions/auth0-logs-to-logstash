e58f339f26ddeb3d854430c109dfd59346d00507
scazarubin <nonexist.void@gmail.com>
2017-06-22 14:02:25 +0300

    bodyparser enabled; webtask.json fixed

--- server/index.js
+++ server/index.js
@@ -30,10 +30,8 @@ module.exports = (configProvider, storageProvider) => {
 
   app.use(processLogs(storage));
 
-  if (process.env.NODE_ENV === 'development') {
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-  }
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
 
   // Configure routes.
   app.use(expressTools.routes.dashboardAdmins({
